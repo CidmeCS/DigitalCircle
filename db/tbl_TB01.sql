d6197ef8215e67f72fc40c1363a086206d69938b
cidme <cidevangelista@hotmail.com>
2022-07-16 17:04:50 -0300

    update table

--- db/tbl_TB01.sql
+++ db/tbl_TB01.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS public."TB01"
 (
-    id integer NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     col_texto "char"[],
     col_dt date,
     CONSTRAINT "TB01_pkey" PRIMARY KEY (id)
